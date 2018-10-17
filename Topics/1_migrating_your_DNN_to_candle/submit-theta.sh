3f31d94935285b7c6e2b60268e55d87473f81e0c
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-10-17 12:23:31 -0500

    New write protection, comments

--- Topics/1_migrating_your_DNN_to_candle/submit-theta.sh
+++ Topics/1_migrating_your_DNN_to_candle/submit-theta.sh
@@ -3,10 +3,12 @@ set -eu
 
 # SUBMIT THETA
 
+# The current directory
 export THIS=$( readlink --canonicalize-existing $( dirname $0 ) )
 
 TIMESTAMP=$( date +%H:%M:%S )
 
+# Editable settings
 export NODES=1
 export PROJECT=ecp-testbed-01
 export QUEUE=debug-cache-quad
@@ -17,7 +19,12 @@ export OUTPUT=$THIS/out-$TIMESTAMP.txt
 # export PROGRAM=$THIS/test-keras.py
 export PROGRAM=$THIS/t29res.py
 
+# Remove write permissions for job-theta to prevent accidental editing
+# of generated version
+touch job-theta.sh
+chmod u+w job-theta.sh
 m4 common.m4 job-theta.sh.m4 > job-theta.sh
-chmod u+x job-theta.sh
+chmod a-w,u+x job-theta.sh
 
+# Submit!
 qsub job-theta.sh
